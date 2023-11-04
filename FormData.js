97a8a25ed9d90b8f2617827ce7e0d66f22babd73
Priyadarshi <priyadarshi@conceptive.co.in>
2023-11-04 19:23:39 +0530

    Chat app demo created

---
+++ FormData.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+let message = "";
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My Page</title></head>");
+    res.write("<body>");
+    res.write(`<p>${message}</p>`);
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text" name="message" /><button>Submit</button>');
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      message = parsedBody.split("=")[1];
+      fs.writeFile("Message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.write("<html>");
+  res.write("<head><title>My Page</title></head>");
+  res.write("<body>");
+  res.write("<h1>Welcome to my Node JS server</h1>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
+});
+server.listen(4000);
