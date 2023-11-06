ba578b80ba48b4f47601f9cacda42ec93cb6ba7e
Priyadarshi <priyadarshi@conceptive.co.in>
2023-11-06 11:21:09 +0530

    created routing to diff url and send data

--- Express/app.js
+++ Express/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="prodName" /><input type="number" name="Qty" /><button type="submit">Submit Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In next middleware");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Welcome to Express</h1>");
 });
 
